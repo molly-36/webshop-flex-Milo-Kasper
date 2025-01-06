f504355c9dc828e77917a9f65e9b9eafcddad5dc
molly-36 <milo.moreels@bazandpoort.be>
2025-01-06 19:26:52 +0100

    vervanging database naar site files

--- site_files/connect.php
+++ site_files/connect.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $username="root";
     $password= "";
-    $dbname= "eerste database";
+    $dbname= "database_webshop";
     $mysqli = new mysqli($host, $username, $password, $dbname);
     
     if ($mysqli->connect_error) {
--- site_files/home.php
+++ site_files/home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylehome.css">
+    <link rel="stylesheet" href="stylesheet_homepage.css">
     <title>Nieuw Nieuws</title>
 </head>
     <?php
@@ -21,5 +21,6 @@
 </ul>
 <body>
     <h1>Welkom!</h1>
+   <button>TEET</button>
 </body>
 </html>
