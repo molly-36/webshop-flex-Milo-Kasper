b30e29f6d33e97be11fa07df560e7db3dcb3e63a
ksprsls <kasper.saelens@bazandpoort.be>
2025-01-20 22:25:03 +0100

    ni veel
    s

--- site_files/register.php
+++ site_files/register.php
@@ -6,71 +6,66 @@
     <link rel="stylesheet" href="registerStyle.css">
     <title>register</title>
 </head>
-<button><a href="index.php"><--- terug</a></button>
+<button id="terugr"><a href="index.php"><--- terug</a></button>
 <body>
     <h1>REGISTER</h1>
-<?php
-    include("connect.php");
-    if (isset($_POST["submit"])) {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        //controleert of er al iemand is met dezelfde user
-        $sql2 = "SELECT * FROM tabel_users WHERE username = '$username'";
-        $result2 = $mysqli->query($sql2);
-        if($result2->num_rows > 0) {
-            echo '
-                <div>
-                <h1 id="h1A">account aanmaken</h1>
-                <form method="post" action="register.php">
-                <i id="slot" class="fa-solid fa-lock"></i>
-                <i id="logo" class="fa-solid fa-user"></i>
-                <input  id="invoer" type="text" name="username" placeholder="            your username"><br>
-                <input id="invoer" type="password" name="password" placeholder="            wachtwoord"><br>
-                <input id="knop" type="submit" name="submit" value="registreer"> 
-                </form><br>
-                <span style="color:red;font-size:100px;background-color:darkred;">user al in gebruik</span>
-                </div>
-            ';
-                
-
-        }else{
-                $sql = "INSERT into tabel_users (username, password) VALUES ('$username', '$password')";
-                $result = $mysqli->query($sql);
-                if ($result) {
-                    header("Location: index.php");
-                }
-                else {
-                    echo '
-                    <div>
-                    <h1 id="h1A">account aanmaken</h1>
-                    <form method="post" action="register.php">
-                    <i id="slot" class="fa-solid fa-lock"></i>
-                    <i id="logo" class="fa-solid fa-user"></i>
-                    <input  id="invoer" type="text" name="username" placeholder="            your username"><br>
-                    <input id="invoer" type="password" name="password" placeholder="            wachtwoord"><br>
-                    <input id="knop" type="submit" name="submit" value="registreer"> 
-                </form><br>
-                <span style="color:red;font-size:100px;background-color:darkred;">er is iets fout gegaan: error 4</span>
-                </div>
-                ';
-                }
-            }
-    }
-    else {
+    <?php
+        include("connect.php");
+        if (isset($_POST["submit"])) {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            // controleert of er al iemand is met dezelfde user
+            $sql2 = "SELECT * FROM tabel_users WHERE username = '$username'";
+            $result2 = $mysqli->query($sql2);
+            if ($result2->num_rows > 0) {
                 echo '
                 <div>
                     <h1 id="h1A">account aanmaken</h1>
                     <form method="post" action="register.php">
+                        <i id="slot" class="fa-solid fa-lock"></i>
+                        <i id="logo" class="fa-solid fa-user"></i>
+                        <input id="invoer" type="text" name="username" placeholder="            your username"><br>
+                        <input id="invoer" type="password" name="password" placeholder="            wachtwoord"><br>
+                        <input id="knop" type="submit" name="submit" value="registreer"> 
+                    </form><br>
+                    <span style="color:red;font-size:100px;background-color:darkred;">user al in gebruik</span>
+                </div>
+                ';
+            } else {
+                $sql = "INSERT into tabel_users (username, password) VALUES ('$username', '$password')";
+                $result = $mysqli->query($sql);
+                if ($result) {
+                    header("Location: index.php");
+                } else {
+                    echo '
+                    <div>
+                        <h1 id="h1A">account aanmaken</h1>
+                        <form method="post" action="register.php">
+                            <i id="slot" class="fa-solid fa-lock"></i>
+                            <i id="logo" class="fa-solid fa-user"></i>
+                            <input id="invoer" type="text" name="username" placeholder="            your username"><br>
+                            <input id="invoer" type="password" name="password" placeholder="            wachtwoord"><br>
+                            <input id="knop" type="submit" name="submit" value="registreer"> 
+                        </form><br>
+                        <span style="color:red;font-size:100px;background-color:darkred;">er is iets fout gegaan: error 4</span>
+                    </div>
+                    ';
+                }
+            }
+        } else {
+            echo '
+            <div>
+                <h1 id="h1A">account aanmaken</h1>
+                <form method="post" action="register.php">
                     <i id="slot" class="fa-solid fa-lock"></i>
                     <i id="logo" class="fa-solid fa-user"></i>
-                    <input  id="invoer" type="text" name="username" placeholder="            your username"><br>
+                    <input id="invoer" type="text" name="username" placeholder="            your username"><br>
                     <input id="invoer" type="password" name="password" placeholder="            wachtwoord"><br>
                     <input id="knop" type="submit" name="submit" value="registreer"> 
                 </form><br>
-                </div>
+            </div>
             ';
-    }
-?>
-       
+        }
+    ?>
 </body>
 </html>
